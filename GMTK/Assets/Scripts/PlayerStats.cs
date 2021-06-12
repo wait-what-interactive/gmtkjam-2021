e3340ce4da42a47a38f176e487bb08d0e1095e66
Andriy Podobailo <47588502+EndL11@users.noreply.github.com>
2021-06-13 00:24:25 +0300

    done showing and changing player HP UI

--- GMTK/Assets/Scripts/PlayerStats.cs
+++ GMTK/Assets/Scripts/PlayerStats.cs
@@ -1,15 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerStats : MonoBehaviour
 {
     private int _hp = 10;
+    private int _maxHP;
+
+    public Image healthBar;
+
+    private void Start()
+    {
+        _maxHP = _hp;
+        healthBar.fillAmount = _maxHP / _hp;
+    }
 
     public void TakeDamage(int damage)
     {
-        _hp -= damage;
-        if(_hp <= 0f)
+        if (_hp - damage < 0)
+            _hp = 0;
+        else
+            _hp -= damage;
+
+        if (_hp > 0)
+            healthBar.fillAmount = (float)_hp / _maxHP;
+        else
+            healthBar.fillAmount = 0;
+
+        Debug.Log("percentage: " + (float)_hp / _maxHP);
+        if (_hp <= 0f)
             Debug.Log("Game over handler here!");
     }
 }
