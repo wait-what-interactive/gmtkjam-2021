9e830c5b94eeba9ad0f56133119035ea6f6f1b98
Vitalii Boiko <boiko_ak18@nuwm.edu.ua>
2021-06-12 17:42:19 +0300

    Added oprions and levels submenus

--- GMTK/Assets/Scripts/MainMenuManager.cs
+++ GMTK/Assets/Scripts/MainMenuManager.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    
+    public void Play()
     {
-        
+        SceneManager.LoadScene("Game");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Exit()
     {
-        
+        Application.Quit();
     }
+
 }
